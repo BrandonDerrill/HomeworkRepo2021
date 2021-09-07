41fd54b7c9d256df188999adbea436e637a04403
Brandon Derrill <Derrillbrandon@gmail.com>
2021-09-07 07:13:55 -0500

    HmWeek.js

---
+++ HmWeek2.js
@@ -0,0 +1,12 @@
+const PizzaPlace = "Hood's Pizza";
+let num1 = 3;
+console.log(num1);
+console.log(typeof PizzaPlace);
+console.log(typeof num1);
+console.log(PizzaPlace);
+console.log(`${PizzaPlace} now offers 1 Large Pizza with ${num1} toppings`);
+let toppings = 10;
+
+//toppings
+//if you offer more than 10 toppings: "A whole lot of pizza."
+if (toppings >= 10 && toppings < 20) console.log(`"A whole lot of pizza."`);
